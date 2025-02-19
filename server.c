00262500f0a6a3f613b1640ea02ea7fccd22db7c
Liza Mozolyuk <lizamozolyuk@gmail.com>
2025-02-19 15:26:50 -0500

    added in code for event loop with epoll()

--- server.c
+++ server.c
@@ -5,7 +5,16 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <assert.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
 
+
+#define MAX_EVENTS 10
+
+void set_nonblocking(int sock) {
+    int flags = fcntl(sock, F_GETFL, 0);
+    fcntl(sock, F_SETFL, flags | O_NONBLOCK);
+}
 
 void panic(char *msg) {
     perror(msg);
@@ -14,7 +23,8 @@ void panic(char *msg) {
 
 int main(void) {
     char *ds = "domain_socket";
-    int socket_desc;
+    int socket_desc, epoll_fd;
+    struct epoll_event event, events[MAX_EVENTS];
 
     socket_desc = domain_socket_server_create(ds);
     if (socket_desc < 0) {
@@ -28,22 +38,49 @@ int main(void) {
 
     /* Handle multiple clients sequentially */
     /* TODO: Liza change this to handle clients concurrently*/
-    while (1) { 
-        int new_client;
-        char b;
+    set_nonblocking(socket_desc);
 
-        new_client = accept(socket_desc, NULL, NULL);
-        if (new_client == -1) panic("server accept");
+    epoll_fd = epoll_create1(0);
+    if (epoll_fd == -1) panic("epoll_create1");
 
-        printf("Client connected!\n");
+    event.data.fd = socket_desc;
+    event.events = EPOLLIN;
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, socket_desc, &event) == -1) panic("epoll_ctl");
 
-        /* Read from, then write to the client */
-        if (read(new_client, &b, 1) == -1) panic("server read");
-        if (write(new_client, "*", 1) == -1) panic("server write");
-        printf("Server read from client: %c\n", b);
-        close(new_client);
+    printf("Server is running and waiting for clients...\n");
+
+    // notes from liza: used chatGPT to aid with event loop creation
+    while (1) {
+        int n, i;
+        n = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+        if (n == -1) panic("epoll_wait");
+
+        for (i = 0; i < n; i++) {
+            if (events[i].data.fd == socket_desc) {
+                int new_client;
+                new_client = accept(socket_desc, NULL, NULL);
+                if (new_client == -1) continue;
+                set_nonblocking(new_client);
+
+                event.data.fd = new_client;
+                event.events = EPOLLIN;
+                epoll_ctl(epoll_fd, EPOLL_CTL_ADD, new_client, &event);
+
+                printf("Client connected!\n");
+            } else {
+                int client_fd = events[i].data.fd;
+                char b;
+                if (read(client_fd, &b, 1) > 0) {
+                    write(client_fd, "*", 1);
+                    printf("Server read from client: %c\n", b);
+                }
+                close(client_fd);
+            }
+        }
     }
 
+    close(socket_desc);
+    close(epoll_fd);
     close(socket_desc);
     printf("Server shutting down...\n");
 
